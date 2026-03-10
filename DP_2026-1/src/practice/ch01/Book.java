901464a58425a6f139b73f11e3ac0617abcf377d
임수영 <christine8739@duksung.ac.kr>
2026-03-10 10:16:41 +0900

    3월 10일 수업종료(01반)

---
+++ DP_2026-1/src/practice/ch01/Book.java
@@ -0,0 +1,13 @@
+package practice.ch01; 
+
+public class Book {
+    private String name; //생성자
+
+    public Book(String name) {
+        this.name = name; // 자기 속성 
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ DP_2026-1/src/practice/ch01/Main.java
@@ -0,0 +1,8 @@
+pacakge practice.ch01;
+
+puplic class Main{
+    public static void main(String[] args) {
+        Book book = new Book("자바의 정석");
+        System.out.println(book.getName());
+    }
+}
